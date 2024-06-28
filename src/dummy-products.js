17190e7d474bb5af5396dae0254f6b3a37415a94
Akshai P R <akshai4610@gmail.com>
2024-06-28 15:11:39 +0530

    Initial Commit

---
+++ src/dummy-products.js
@@ -0,0 +1,58 @@
+import mochaOvercoat from './assets/GamePicture_1.jpeg';
+import dreamGown from './assets/GamePicture_2.jpg';
+import swordGuy from './assets/GamePicture_3.jpg';
+import merlotSuit from './assets/GamePicture_4.jpg';
+import FarCry from './assets/GamePicture_5.jpg';
+import denimPioneer from './assets/GamePicture_6.jpeg';
+
+export const DUMMY_PRODUCTS = [
+  {
+    id: 'p1',
+    image: mochaOvercoat,
+    title: 'Majestic Vintage Mocha Overcoat',
+    price: 1029.99,
+    description:
+      'Channel timeless sophistication with this stunning mocha overcoat. Crafted for the discerning gentleman who appreciates the fine blend of vintage charm and modern elegance.',
+  },
+  {
+    id: 'p2',
+    image: dreamGown,
+    title: 'Enchanting Blush Dream Gown',
+    price: 1089.99,
+    description:
+      'Bask in the glow of elegance with our Enchanting Blush Dream Gown. Embody the regality of a queen with a sweet touch of pink that whispers enchantment. This is the perfect piece for those seeking to create unforgettable moments.',
+  },
+
+  {
+    id: 'p3',
+    image: swordGuy,
+    title: 'Vintage Sunshine Sword Guy',
+    price: 1149.99,
+    description:
+      'Brace the showers in style! Our Vintage Sunshine Rain Jacket ensures that you stand out, even in the dullest weather. Because rain is never a reason to compromise on your fashion quotient.',
+  },
+  {
+    id: 'p4',
+    image: merlotSuit,
+    title: 'Classic Merlot Suit',
+    price: 1249.99,
+    description:
+    'Step into the boardroom with unmatched confidence in our Classic Merlot Business Suit. Exuding an air of refined class and understated power, it is ideal for the modern executive who values tradition and elegance.',
+    },
+    {
+    id: 'p5',
+    image: FarCry,
+    title: 'Ethereal Moonlight Evening FarCry edition',
+    price: 1159.99,
+    description:
+    'Sweep the room off its feet in our Ethereal Moonlight Evening Dress. Crafted to mimic the allure of the moonlight, this dress is a nod to those who appreciate subtle glamour and a standout silhouette.',
+    },
+    {
+    id: 'p6',
+    image: denimPioneer,
+    title: 'Pioneer Rugged Denim ',
+    price: 1279.99,
+    description:
+    'Our Pioneer Rugged Denim Jacket is a tribute to those who embody the spirit of adventure. Designed with durability and comfort in mind, this jacket is a wardrobe essential for the urban explorer.'
+    }
+];
